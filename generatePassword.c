a1845bff58538a09ecd15b8790b290669c27bf91
DevNitro5 <anishchhetri196@gmail.com>
2023-03-15 09:29:58 +0000

    fix bug

--- generatePassword.c
+++ generatePassword.c
@@ -9,20 +9,20 @@ typedef struct
 
 void generate_password(char **password)
 {
-    Range ranges[] = {{64, 90}, {97, 122}, {48, 57}, {35, 38}};
+    Range ranges[] = {{64, 90}, {97, 122}, {48, 57}, {33, 38}};
     Range selected_range;
-    int length;
     int index;
     int i;
+    int ranges_length = sizeof(ranges) / sizeof(Range);
 
-    printf("\nEnter the length of the password => ");
-    scanf("%d", &length);
+    printf("\nEnter the password_length of the password => ");
+    int password_length = get_int();
 
-    (*password) = (char *)malloc(sizeof(char) * (length + 1));
+    (*password) = (char *)malloc(sizeof(char) * (password_length + 1));
 
-    for (i = 0; length > 0; i++, length--)
+    for (i = 0; password_length > 0; i++, password_length--)
     {
-        index = generate_random_number(0, sizeof(ranges) / sizeof(Range));
+        index = generate_random_number(0, ranges_length - 1);
         selected_range = ranges[index];
 
         (*password)[i] = generate_random_number(selected_range.from, selected_range.to);
